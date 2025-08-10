4df5585fee646cdc5f8edf04c06bb463d5b77775
palchikmrg <palchik.mrg@gmail.com>
2025-08-10 22:12:27 +0300

    reversed-strings.js

---
+++ 8kyu/reversed-strings.js
@@ -0,0 +1,16 @@
+// INSTRUCTIONS
+// Complete the solution so that it reverses the string passed into it.
+//
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+
+//SOLUTION
+function solution(str){
+    let str2 = ""
+    for(let i = str.length - 1; i >= 0; i-- ) {
+        str2 = str2 + str[i]
+    }
+    return str2
+
+}
